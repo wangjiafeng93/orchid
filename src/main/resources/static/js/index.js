345fc42fba46a8190c7dd530744913eafec795d5
wangjiafeng <wangjiafeng@thunisoft.com>
2020-03-30 18:39:59 +0800

    新建-项目新建

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,15 @@
+// **********************************************//
+// index 客户端脚本
+//
+// @author huayu
+// @date 2020-03-30
+// **********************************************//
+
+var vm = new Vue({
+    el: '#app',
+    mixins: [atyTplMixin],
+    data: function () {
+        return {}
+    },
+    methods: {}
+})
